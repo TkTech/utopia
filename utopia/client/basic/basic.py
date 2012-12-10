9318bfd81a9b9aa451f85b0af32337d616c8f48d
Tyler Kennedy <tk@tkte.ch>
2012-12-10 03:29:38 -0500

    Simplified the concept of callbacks.

--- utopia/client/basic/basic.py
+++ utopia/client/basic/basic.py
@@ -22,7 +22,7 @@ class BasicClient(CoreClient):
         self._realname = kwargs.pop('realname', None)
 
         super(BasicClient, self).__init__(*args, **kwargs)
-        self._callbacks = defaultdict(set)
+        self._callbacks = set([self])
         self._channels = {}
 
     def connect(self, *args, **kwargs):
@@ -35,13 +35,16 @@ class BasicClient(CoreClient):
         exists.
         """
         command = message.command.lower()
-        for callback in self._callbacks[command]:
-            gevent.spawn(callback, callback, message)
+        for callback in self._callbacks:
+            handler = getattr(callback, 'message_{command}'.format(
+                command=command
+            ), None)
+            # If the callback has a catch-all, use it.
+            if handler is None and hasattr(callback, 'message_not_handled'):
+                handler = callback.message_not_handled
 
-        handler = getattr(self, 'message_{command}'.format(
-            command=command
-        ), self.message_not_handled)
-        gevent.spawn(handler, message)
+            if handler is not None:
+                gevent.spawn(handler, message)
 
     def message_not_handled(self, message):
         """
@@ -64,22 +67,22 @@ class BasicClient(CoreClient):
         """
         self.send('PONG', *message.args)
 
-    def collect(self, *message_types):
+    def collect(self, *args, **kwargs):
         """
         A helper that sets up a new `MessageCollector` to collect messages of
         `message_types` and returns it.
         """
         q = MessageCollector(self)
-        q.collect(message_types)
+        q.collect(*args, **kwargs)
         return q
 
-    def wait_for(self, *message_types):
+    def wait_for(self, *args, **kwargs):
         """
         A helper that sets up a new `MessageCollector` to wait for messages of
         `message_types` and returns it.
         """
         q = MessageCollector(self)
-        q.wait_for(message_types)
+        q.wait_for(*args, **kwargs)
         return q
 
     def channel(self, channel_name):
@@ -89,3 +92,5 @@ class BasicClient(CoreClient):
         if channel_name not in self._channels:
             self._channels[channel_name] = Channel(self, channel_name)
         return self._channels[channel_name]
+
+    __getitem__ = channel
--- utopia/client/basic/channel.py
+++ utopia/client/basic/channel.py
@@ -8,42 +8,25 @@ class Channel(object):
     def __init__(self, client, name):
         self._name = name
         self._client = client
+        self._users = {}
 
     @property
     def client(self):
         return self._client
 
     @property
-    def channel_name(self):
+    def channel(self):
         return self._name
 
-    def names(self):
+    def join(self, password=None):
         """
-        Returns a list of all the users in this channel.
-        """
-        with self.collect(353, 366).wait_for(366) as ar:
-            self.send('NAMES', self._name)
-
-            final_names = set()
-
-            for message in ar.get():
-                if message.command == '353':
-                    to, chan_mode, for_channel, names = message.args
-                    if for_channel != self._name:
-                        # This was a listing for a channel other than the
-                        # one requested.
-                        continue
-
-                    final_names.update(names.split())
-
-            return final_names
-
-    def join(self, key=None):
-        """
-        Joins the channel and waits until it has recieved the names list
+        Attempts to join and waits until it has recieved the names list
         (or an error) before returning.
+
+        :param password: The channel password (key) if needed to join.
         """
-        responses = (
+        # Possible error responses from JOIN.
+        errors = (
             Errors.NEEDMOREPARAMS,
             Errors.INVITEONLYCHAN,
             Errors.CHANNELISFULL,
@@ -51,20 +34,22 @@ class Channel(object):
             Errors.TOOMANYTARGETS,
             Errors.BANNEDFROMCHAN,
             Errors.BADCHANNELKEY,
-            Errors.TOOMANYCHANNELS,
-            Errors.UNAVAILRESOURCE,
-            Responses.ENDOFNAMES
+            Errors.TOOMANYCHANNELS
+        )
+        success = (
+            Responses.ENDOFNAMES,
         )
 
-        with self.client.collect(*responses).wait_for(*responses) as ar:
-            if key:
-                self.client.send('JOIN', self.channel_name, key)
+        q = self.client.wait_for(
+            *success, f=lambda m: m.args[1] == self.channel
+        ).errors(
+            *errors, f=lambda m: m.args[0] == self.channel
+        )
+
+        with q:
+            if password:
+                self.client.send('JOIN', self.channel, password)
             else:
-                self.client.send('JOIN', self.channel_name)
+                self.client.send('JOIN', self.channel)
 
-            results = []
-            for message in ar.get():
-                if message.args[0] == self.channel_name:
-                    results.append(message)
-
-            return results
+            return q.get()
