0e29bd42866fd383e2de3e2375b13f3a2147abdc
Tyler Kennedy <tk@tkte.ch>
2013-02-18 17:04:43 -0500

    bugfix version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="UtopiaIRC",
-    version="0.1.6",
+    version="0.1.7",
     description="Simple gevent-based IRC bot, because why not.",
     url="http://github.com/TkTech/utopia",
     author="Tyler Kennedy",
